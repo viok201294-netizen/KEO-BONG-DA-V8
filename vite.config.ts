62511a5e7ac66c56ebbe5f3227d754e7c23e6895
Vinh Trịnh <viok201294@gmail.com>
2026-03-18 00:46:42 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/KEO-BONG-DA-V8/', // Cấu hình đường dẫn cho GitHub Pages
+    base: './', // Cấu hình đường dẫn cho GitHub Pages
     plugins: [
       react(),
       tailwindcss()
